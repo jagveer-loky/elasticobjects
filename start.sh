0922c1d682c0834f19f9fda92f83df96830c9294
Diwischek <fluentcodes@gmail.com>
2020-10-13 15:21:37 +0200

    V0.5.0 Finished

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-java -cp example-springboot/target/example-springboot-0.5.0-SNAPSHO.jar:example-springboot/input/ org.springframework.boot.loader.PropertiesLauncher
+java -cp example-springboot/target/example-springboot-0.5.0.jar:example-springboot/input/ org.springframework.boot.loader.PropertiesLauncher
