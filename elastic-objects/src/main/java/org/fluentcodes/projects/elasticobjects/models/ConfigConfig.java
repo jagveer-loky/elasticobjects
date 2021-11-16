dc0a9af52fbf69b80019c13bfa192ac26c2441b9
Werner Diwischek <werner.diwischek@1und1.de>
2021-11-16 04:03:55 +0100

    V0.8.1 add populate bean

--- elastic-objects/src/main/java/org/fluentcodes/projects/elasticobjects/models/ConfigConfig.java
+++ elastic-objects/src/main/java/org/fluentcodes/projects/elasticobjects/models/ConfigConfig.java
@@ -1,71 +1,84 @@
 package org.fluentcodes.projects.elasticobjects.models;
 
-import java.util.Map;
 import org.fluentcodes.projects.elasticobjects.UnmodifiableMap;
-
-/*=>{javaHeader}|*/
-import java.util.List;
 import org.fluentcodes.projects.elasticobjects.domain.BaseConfig;
 import org.fluentcodes.projects.elasticobjects.exceptions.EoInternalException;
 
+import java.util.List;
+import java.util.Map;
+
 /**
- * 
- * Basic cache as super object for other cached items.  
+ * Basic cache as super object for other cached items.
+ *
  * @author Werner Diwischek
  * @creationDate null
  * @modificationDate Sun Jan 10 11:32:02 CET 2021
  */
 public class ConfigConfig extends BaseConfig implements ConfigInterface {
-/*=>{}.*/
-   private final Map properties;
-    /*=>{javaInstanceVars}|*/
-   /* expose */
-   private final Expose expose;
-   /* Defines a target module where generating occurs.  */
-   private final String module;
-   /* Defines scope of the configuration within module, eg 'test' or 'main' . */
-   private final String moduleScope;
-   /* A scope for the cache value. */
-   private final List<Scope> scope;
-/*=>{}.*/
+  /*=>{}.*/
+  private final Map<String, Object> properties;
+  /*=>{javaInstanceVars}|*/
+  /* expose */
+  private final Expose expose;
+  /* Defines a target module where generating occurs.  */
+  private final String module;
+  /* Defines scope of the configuration within module, eg 'test' or 'main' . */
+  private final String moduleScope;
+  /* A scope for the cache value. */
+  private final List<Scope> scope;
+  /*=>{}.*/
 
-    public ConfigConfig(ConfigBean configBean) {
-        super(configBean);
-        this.module = configBean.getModule();
-        this.moduleScope = configBean.getModuleScope();
-        this.scope = configBean.getScope();
-        this.expose = configBean.getExpose();
-        if (configBean.getProperties() == null) {
-            throw new EoInternalException("Null properties not allowed creating configs.");
-        }
-        this.properties = new UnmodifiableMap(configBean.getProperties());
+  public ConfigConfig(ConfigBean configBean) {
+    super(configBean);
+    this.module = configBean.getModule();
+    this.moduleScope = configBean.getModuleScope();
+    this.scope = configBean.getScope();
+    this.expose = configBean.getExpose();
+    if (configBean.getProperties() == null) {
+      throw new EoInternalException("Null properties not allowed creating configs.");
     }
+    this.properties = new UnmodifiableMap<>(configBean.getProperties());
+  }
 
-    @Override
-    public Map<String, Object> getProperties() {
-        return properties;
-    }
+  @Override
+  public Map<String, Object> getProperties() {
+    return properties;
+  }
 
-    /*=>{javaAccessors}|*/
-   @Override
-   public Expose getExpose() {
-      return this.expose;
-   }
+  /*=>{javaAccessors}|*/
+  @Override
+  public Expose getExpose() {
+    return this.expose;
+  }
 
-   @Override
-   public String getModule() {
-      return this.module;
-   }
+  @Override
+  public String getModule() {
+    return this.module;
+  }
 
-   @Override
-   public String getModuleScope() {
-      return this.moduleScope;
-   }
+  @Override
+  public String getModuleScope() {
+    return this.moduleScope;
+  }
 
-   @Override
-   public List<Scope> getScope() {
-      return this.scope;
-   }
+  @Override
+  public List<Scope> getScope() {
+    return this.scope;
+  }
 
-/*=>{}.*/
+  /*=>{}.*/
+
+  /**
+   * Set the values from config to {@link ConfigBean}
+   *
+   * @param bean basebean
+   */
+  public void populateBean(ConfigBean bean) {
+    super.populateBean(bean);
+    bean.setConfigModelKey(this.getClass().getSimpleName());
+    bean.setExpose(getExpose());
+    bean.setModule(getModule());
+    bean.setModuleScope(getModuleScope());
+    bean.setScope(getScope());
+  }
 }
