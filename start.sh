a35db69b6d90ef32ecb0f418c2f2b591e404d8fd
Diwischek <fluentcodes@gmail.com>
2021-11-19 17:40:44 +0100

    V0.8.1 ready

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-java -cp example-springboot/target/example-springboot-0.8.1-SNAPSHOT.jar:example-springboot/input/ org.springframework.boot.loader.PropertiesLauncher
+java -cp example-springboot/target/example-springboot-0.8.1.jar:example-springboot/input/ org.springframework.boot.loader.PropertiesLauncher
