9b56661db2bbea3d9e624de5552d5d1a6bd1bded
Diwischek <fluentcodes@gmail.com>
2021-12-02 05:54:54 +0100

    V0.8.2 Remove builder folders

--- builder/src/main/java/org/fluentcodes/projects/elasticobjects/models/FieldBeanInterface4Javascript.java
+++
@@ -1,5 +0,0 @@
-package org.fluentcodes.projects.elasticobjects.models;
-
-public interface FieldBeanInterface4Javascript extends FieldInterface {
-
-}
--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-java -cp example-springboot/target/example-springboot-0.8.2.jar:example-springboot/input/ org.springframework.boot.loader.PropertiesLauncher
+java -cp example-springboot/target/example-springboot-0.9.0.jar:example-springboot/input/ org.springframework.boot.loader.PropertiesLauncher
